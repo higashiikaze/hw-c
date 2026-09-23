6dc0132c67283c7af81aab36d4d5b0cdda8b0bb8
higashiikaze <jeremeewa@yahoo.com>
2026-09-23 19:47:21 +0300

    HW2

---
+++ hw_2/equiv.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("  A  B  A<->B\n");
+	printf("  0  0    1\n");
+	printf("  0  1    0\n");
+	printf("  1  0    0\n");
+	printf("  1  1    1\n");
+
+	return 0;
+}
